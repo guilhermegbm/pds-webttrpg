d2ea079d2ad87c5092dfe62b7ed1f9ff8fdf14d5
lucas campos <lucascampos.mg@gmail.com>
2023-04-14 21:38:40 -0300

    adiciona novas colunas ao schema da tabela de token de autenticação

--- backend/src/database_script/generate_schema.sql
+++ backend/src/database_script/generate_schema.sql
@@ -9,5 +9,7 @@ create table webttrpg.user (
 create table webttrpg.authentication_token (
     token varchar(100) primary key not null,
     user_id varchar (100) not null,
+    created_at timestamp not null,
+    expiration_time_in_hours int not null,
     constraint authentication_token_user_fk foreign key (user_id) references webttrpg.user(id)
-)
+);
