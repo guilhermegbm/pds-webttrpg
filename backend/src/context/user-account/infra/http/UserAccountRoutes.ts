be04961a94c573bb92c70aaa8f910d7c6a8b02f7
lucas campos <lucascampos.mg@gmail.com>
2023-04-20 23:12:42 -0300

    define arquivo para configuração das rotas do contexto de conta de usuário

---
+++ backend/src/context/user-account/infra/http/UserAccountRoutes.ts
@@ -0,0 +1,9 @@
+import Server, { HttpMethod } from "../../../../infra/http/Server";
+import UserAccountController from "./UserAccountController";
+
+export default class UserAccountRoutes {
+
+    static defineRoutes(server: Server) {
+        server.on(HttpMethod.POST, "/signin", UserAccountController.signin);
+    }
+}
