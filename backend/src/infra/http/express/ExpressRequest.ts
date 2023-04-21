5ffe46f28e6046e6dbf7635492ff3dd1c262fc34
lucas campos <lucascampos.mg@gmail.com>
2023-04-20 22:54:05 -0300

    adiciona implementação do servidor http rest baseado no framework express

---
+++ backend/src/infra/http/express/ExpressRequest.ts
@@ -0,0 +1,16 @@
+import { Request as RequestExpress} from "express";
+import Request from "../Request";
+
+export default class ExpressRequest implements Request {
+    private request: RequestExpress;
+
+    constructor(request: RequestExpress) {
+        this.request = request;
+    }
+    getParams() {
+        return this.request.params;
+    }
+    getBody() {
+        return this.request.body;
+    }
+}
---
+++ backend/src/infra/http/express/ExpressResponse.ts
@@ -0,0 +1,17 @@
+import { Response as ResponseExpress} from "express";
+import Response from "../Response";
+
+export default class ExpressResponse implements Response {    
+    private response: ResponseExpress;
+    
+    constructor(response: ResponseExpress) {
+        this.response = response;
+    }
+    status(httpStatus: number): Response {
+        this.response.status(httpStatus);
+        return this;
+    }
+    send(result: object): void {
+        this.response.send({ ... result});
+    }
+}
