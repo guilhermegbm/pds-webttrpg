9b04665f16cda43da789dc3179e10424d3d25abc
lucas campos <lucascampos.mg@gmail.com>
2023-04-23 16:14:21 -0300

    adiciona operação de verificar se um usuário existe e persistir no repositorio

--- backend/src/context/user-account/domain/collection/UserCollection.ts
+++ backend/src/context/user-account/domain/collection/UserCollection.ts
@@ -2,4 +2,6 @@ import User from "../entity/User";
 
 export default interface UserCollection {
     getByUsername(username: string): Promise<User>;
+    existByUsername(username: string): Promise<boolean>
+    save(user: User): Promise<void>;
 }
--- backend/src/context/user-account/infra/repository/UserRepository.ts
+++ backend/src/context/user-account/infra/repository/UserRepository.ts
@@ -7,7 +7,23 @@ export default class UserRepository implements UserCollection {
     
     async getByUsername(username: string): Promise<User> {
         const userData = await database.oneOrNone("select * from webttrpg.user where username = $1", [username]);
+        if (!userData) {
+            throw new Error("user not found");
+        }
         const user = UserFactory.create(userData.id, userData.username, userData.password);
         return user;
+    }
+
+    async save(user: User): Promise<void> {
+        await database.none("insert into webttrpg.user (id, username, password) values ($1, $2, $3)", [
+            user.getId(),
+            user.getUsername(),
+            user.getEncryptedPassword()
+        ]);
+    }
+
+    async existByUsername(username: string): Promise<boolean> {
+        const data = await database.oneOrNone("select count(*) from webttrpg.user where username = $1", [username]);
+        return data.count !== "0" ? true : false;
     }
 }
