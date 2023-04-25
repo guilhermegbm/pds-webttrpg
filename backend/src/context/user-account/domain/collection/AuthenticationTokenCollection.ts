d73e3f48a916ade8fd5f3ab71a0a34936b2d407d
lucas campos <lucascampos.mg@gmail.com>
2023-04-24 22:38:33 -0300

    adiciona método na coleção de tokens de autenticação para retornar uma entidade pelo token

--- backend/src/context/user-account/domain/collection/AuthenticationTokenCollection.ts
+++ backend/src/context/user-account/domain/collection/AuthenticationTokenCollection.ts
@@ -2,4 +2,5 @@ import AuthenticationToken from "../entity/AuthenticationToken";
 
 export default interface AuthenticationTokenCollection {
     registerAuthenticationToken(authenticationToken: AuthenticationToken): Promise<void>;
+    getByToken(authenticationToken: string): Promise<AuthenticationToken>;
 }
