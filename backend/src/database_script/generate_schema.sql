2dde2b0f36d4d975fc822648076b85d9f8fe9d39
lucas campos <lucascampos.mg@gmail.com>
2023-04-26 15:20:59 -0300

    adiciona script para definição da tabela de jogos

--- backend/src/database_script/generate_schema.sql
+++ backend/src/database_script/generate_schema.sql
@@ -8,8 +8,17 @@ create table webttrpg.user (
 
 create table webttrpg.authentication_token (
     token varchar(100) primary key not null,
-    user_id varchar (100) not null,
+    user_id varchar(100) not null,
     created_at timestamp not null,
     expiration_time_in_hours int not null,
     constraint authentication_token_user_fk foreign key (user_id) references webttrpg.user(id)
+);
+
+create table webttrpg.game (
+    id varchar(100) primary key not null,
+    name varchar(100) not null unique,
+    maximum_players int not null,
+    description varchar(200) not null,
+    user_id varchar(100) not null,
+    constraint game_user_fk foreign key (user_id) references webttrpg.user(id)
 );
