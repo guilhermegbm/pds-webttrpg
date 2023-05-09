92c2e9e22281861fb29a40edeee566434f1b0d06
Ábner Neves <abnermneves@gmail.com>
2023-05-08 22:34:01 -0300

    iniciando page sign up

---
+++ frontend/src/pages/Cadastro.vue
@@ -0,0 +1,27 @@
+<template>
+    <q-page class="">
+        <q-card>
+            <q-form>
+                <q-input v-model="username" label="Username"/>
+                <q-input v-model="email" label="E-mail"/>
+                <q-input v-model="password" label="Password"/>
+                <q-btn label="Sign up" />
+
+                <p>Alraedy have an account? <a href="" class="bold">Log in now</a></p>
+            </q-form>
+        </q-card>
+    </q-page>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      username: '',
+      email: '',
+      password: ''
+    }
+  }
+}
+
+</script>
