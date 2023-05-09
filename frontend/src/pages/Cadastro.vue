f741bb5e76195a112cfa1b52c52452ce0d918e74
root <root@vulnicura.localdomain>
2023-05-08 22:50:32 -0300

    começando pagae sign up

--- frontend/src/pages/Cadastro.vue
+++ frontend/src/pages/Cadastro.vue
@@ -1,13 +1,13 @@
 <template>
-    <q-page class="">
-        <q-card>
-            <q-form>
+    <q-page class="q-pa-md">
+        <q-card class="">
+            <q-form class="q-pa-md">
                 <q-input v-model="username" label="Username"/>
                 <q-input v-model="email" label="E-mail"/>
                 <q-input v-model="password" label="Password"/>
                 <q-btn label="Sign up" />
 
-                <p>Alraedy have an account? <a href="" class="bold">Log in now</a></p>
+                <p>Already have an account? <a href="" class="bold">Log in now</a></p>
             </q-form>
         </q-card>
     </q-page>
