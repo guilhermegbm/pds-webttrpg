48b6480c5bb9051caacb311dc98485c6f0b58daf
Ábner Neves <abnermneves@gmail.com>
2023-05-21 13:39:57 -0300

    estilizando cadastro

--- frontend/src/pages/Cadastro.vue
+++ frontend/src/pages/Cadastro.vue
@@ -1,16 +1,16 @@
 <template>
-    <q-page class="q-pa-md">
-        <q-card class="">
-            <q-form class="q-pa-md">
-                <q-input v-model="username" label="Username"/>
-                <q-input v-model="email" label="E-mail"/>
-                <q-input v-model="password" label="Password"/>
-                <q-btn label="Sign up" />
-
-                <p>Already have an account? <a href="" class="bold">Log in now</a></p>
-            </q-form>
-        </q-card>
-    </q-page>
+  <q-page class="q-pa-md bg-cinza column justify-center items-center">
+    <q-card class="q-pa-xl bg-vermelho rounded-borders column justify-center">
+      <p class="dragonsden">Dragon's Den</p>
+      <q-form class="q-px-xl column justify-center">
+        <q-input dense rounded outlined v-model="username" label="Username" bg-color="grey-4" class="q-mb-lg"/>
+        <q-input dense rounded outlined v-model="email" label="E-mail" bg-color="grey-4" class="q-mb-lg"/>
+        <q-input dense rounded outlined v-model="password" label="Password" bg-color="grey-4" class="q-mb-lg"/>
+        <q-btn rounded color="cinza" label="Sign up" class="q-mb-lg"/>
+      </q-form>
+      <p class="text-center">Already have an account? <a href="" class="link-login text-bold">Log in now</a></p>
+    </q-card>
+  </q-page>
 </template>
 
 <script>
