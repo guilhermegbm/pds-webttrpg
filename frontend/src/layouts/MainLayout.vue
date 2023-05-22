75a08cb3b4b94933758eae8a7a0097d6979c6910
Ábner Neves <abnermneves@gmail.com>
2023-05-22 18:15:18 -0300

    testando docker

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout view="lHh lpr lFf">
     <q-header elevated>
       <q-toolbar>
         <q-btn
--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -7,7 +7,7 @@
         <q-input dense rounded outlined v-model="password" label="Password" bg-color="grey-4" class="q-mb-lg"/>
         <q-btn rounded color="cinza" label="Log in" class="q-mb-lg"/>
       </q-form>
-      <p class="text-center">Don't have an account yet? <a href="" class="link-signup text-bold">Sign up now</a></p>
+      <p class="text-center">Don't have an account yet? <a href="signup" class="link-signup text-bold">Sign up now</a></p>
     </q-card>
   </q-page>
 </template>
