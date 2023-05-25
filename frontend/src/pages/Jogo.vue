fe08fa4803ceacf21df4690dd728376c63ea65ba
Joshark <johnnytrindade13@hotmail.com>
2023-05-25 09:32:48 -0300

    adds very simple chat layout and tabs to switch from chat to sheets

--- frontend/src/pages/Jogo.vue
+++ frontend/src/pages/Jogo.vue
@@ -1,32 +1,46 @@
 <template>
   <q-layout>
     <q-page-container>
-      <q-page class="flex flex-center row">
+      <q-page class="flex flex-stretch row">
         <div class="flex flex-center col-9">Map</div>
         <q-separator vertical size="4px" color="blue" />
-        <div class="flex flex-center col">
-          <q-form
-            class="q-pa-md row justify-center"
-            @submit="onSend"
+        <div
+          class="column col"
+        >
+          <q-tabs
+            v-model="tab"
+            dense
+            shrink
+            class="bg-grey-3"
+            narrow-indicator
           >
-            <div style="width: 100%; max-width: 400px">
+            <q-tab name="chat" label="Chat" />
+            <q-tab name="sheets" label="Sheets" />
+            <q-tab name="config" label="Config" />
+          </q-tabs>
+
+          <q-tab-panels class="q-pa-md" v-model="tab" animated>
+            <q-tab-panel name="chat">
               <q-chat-message
-                :text="['hey, how are you?']"
-                sent
+                v-for="message in messages"
+                :key="message.id"
+                :text="message.text"
+                :sent="message.sent" />
+              <q-input
+                name="newMessage"
+                square
+                outlined
+                type="textarea"
+                v-model="newMessage"
+                autogrow
+                @keydown.enter.prevent="onSend()"
               />
-              <q-chat-message
-                :text="['doing fine, how r you?']"
-              />
-            </div>
-            <input
-              class="col"
-              type="text"
-              v-model="message"
-              label="Envie uma mensagem"
-            />
-            <input type="submit" hidden />
-          </q-form>
-        </div>
+            </q-tab-panel>
+            <q-tab-panel name="sheets">
+              <q-btn color="primary" label="Add" onclick="addItem()"/>
+            </q-tab-panel>
+          </q-tab-panels>
+      </div>
       </q-page>
     </q-page-container>
   </q-layout>
@@ -37,14 +51,40 @@ export default {
   name: 'Jogo',
   data () {
     return {
+      tab: 'chat',
       chatMessages: null,
-      message: null
+      newMessage: null,
+      messages: [
+        {
+          id: 1,
+          text: ['This is a message'],
+          sent: true
+        },
+        {
+          id: 2,
+          text: ['Hello how is it going?'],
+          sent: false
+        }
+      ]
     }
   },
   methods: {
     onSend () {
-      console.log(this.message)
-      this.message = ''
+      if (this.newMessage === null) {
+        return
+      }
+
+      this.messages.push(
+        {
+          id: this.messages.length + 1,
+          text: [this.newMessage],
+          sent: true
+        }
+      )
+      this.newMessage = null
+    },
+    addItem() {
+      console.log("Adding item")
     }
   }
 }
