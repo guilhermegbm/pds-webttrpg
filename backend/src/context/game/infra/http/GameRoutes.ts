7cba7af7703bcd3dcdeaf53810abf66f7744148f
lucas campos <lucascampos.mg@gmail.com>
2023-05-25 22:51:13 -0300

    add rota que chama a ação que retorna todos os jogadores de um jogo

--- backend/src/context/game/infra/http/GameRoutes.ts
+++ backend/src/context/game/infra/http/GameRoutes.ts
@@ -1,9 +1,11 @@
 import Server, { HttpMethod } from "../../../../infra/http/Server";
 import HttpRestCreateGame from "./HttpRestCreateGame";
+import HttpRestGetAllPlayersByGame from "./HttpRestGetAllPlayersByGame";
 
 export default class GameRoutes {
 
     static defineRoutes(server: Server) {
         server.on(HttpMethod.POST, "/game", new HttpRestCreateGame());
+        server.on(HttpMethod.GET, "/game/:id/players", new HttpRestGetAllPlayersByGame());
     }
 }
