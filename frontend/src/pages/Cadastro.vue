968524045b3b25ed0772ad6cd608e9a1228d13f2
Ábner Neves <abnermneves@gmail.com>
2023-05-28 03:24:18 -0300

    estrutura do vuex funcionando

--- frontend/src/pages/Cadastro.vue
+++ frontend/src/pages/Cadastro.vue
@@ -4,22 +4,31 @@
       <p class="dragonsden">Dragon's Den</p>
       <q-form class="q-px-xl column justify-center">
         <q-input dense rounded outlined v-model="username" label="Username" bg-color="grey-4" class="q-mb-lg"/>
-        <q-input dense rounded outlined v-model="email" label="E-mail" bg-color="grey-4" class="q-mb-lg"/>
         <q-input dense rounded outlined v-model="password" label="Password" bg-color="grey-4" class="q-mb-lg"/>
-        <q-btn rounded color="cinza" label="Sign up" class="q-mb-lg"/>
+        <q-input dense rounded outlined v-model="confirmationPassword" label="Confirm password" bg-color="grey-4" class="q-mb-lg"/>
+        <q-btn rounded color="cinza" label="Sign up" class="q-mb-lg" @click="signUp()"/>
       </q-form>
       <p class="text-center">Already have an account? <a href="signin" class="link-login text-bold">Log in now</a></p>
+      {{ $store.state }}
     </q-card>
   </q-page>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+
 export default {
   data () {
     return {
       username: '',
-      email: '',
-      password: ''
+      password: '',
+      confirmationPassword: ''
+    }
+  },
+  methods: {
+    ...mapActions('storeUser', ['signUp']),
+    signUp () {
+      this.signUp(this.username, this.password, this.confirmationPassword)
     }
   }
 }
