e2473492587f4c4e9aadff1e8a74dc395597dc9a
Guilherme Barboza Mendonça <guilhermebmendonca60@gmail.com>
2023-05-28 00:41:24 -0300

    Correcoes visuais no Header

--- frontend/src/components/dashboard/DashboardHeader.vue
+++ frontend/src/components/dashboard/DashboardHeader.vue
@@ -1,24 +1,18 @@
 <template>
-  <div class="row">
-    <div class="col-12">
 
-      <div id="header" class="row">
-        <div class="col-11">
-          <p class="app-name" style="margin: 0px">
-            Dragon's Den
-          </p>
-        </div>
-        <div class="col-1">
-          <q-btn round color="primary" style="float: right;">
-            <q-avatar size="40px" font-size="20px" color="primary" text-color="white">G</q-avatar>
-          </q-btn>
-        </div>
-      </div>
-
-      <hr style="margin: 0px 50px 0px 50px;" />
-
+  <div id="header" class="row">
+    <div class="col-11">
+      <p class="app-name" style="margin: 0px">
+        Dragon's Den
+      </p>
+    </div>
+    <div class="col-1">
+      <q-btn round color="primary" style="float: right;">
+        <q-avatar size="40px" font-size="20px" color="primary" text-color="white">G</q-avatar>
+      </q-btn>
     </div>
   </div>
+
 </template>
 
 <script>
@@ -31,7 +25,7 @@ export default {
 #header {
   min-height: 50px;
   margin: 0px 50px 0px 50px;
-   align-items: center;
+  align-items: center;
 }
 
 .app-name {
