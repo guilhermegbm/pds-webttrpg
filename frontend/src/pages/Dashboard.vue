32464fa0e7f464aba025b2fe7a2758a4fd3e299d
Guilherme Barboza Mendonça <guilhermebmendonca60@gmail.com>
2023-05-28 00:41:51 -0300

    Adicao da secao de "Options" do Dashboard

--- frontend/src/pages/Dashboard.vue
+++ frontend/src/pages/Dashboard.vue
@@ -2,7 +2,19 @@
   <div id="main" class="row">
     <div class="col-12">
 
-      <DashboardHeader />
+      <div class="row">
+        <div class="col-12">
+          <DashboardHeader />
+        </div>
+      </div>
+
+      <hr style="margin: 0px 50px 0px 50px;" />
+
+      <div class="row">
+        <div class="col-12">
+          <DashboardOptions />
+        </div>
+      </div>
 
     </div>
   </div>
@@ -11,10 +23,12 @@
 <script>
 
 import DashboardHeader from 'src/components/dashboard/DashboardHeader.vue'
+import DashboardOptions from 'src/components/dashboard/DashboardOptions.vue'
 
 export default {
   components: {
-    DashboardHeader
+    DashboardHeader,
+    DashboardOptions
   },
   data () {
     return {
