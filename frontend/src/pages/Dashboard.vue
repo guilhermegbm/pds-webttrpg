cdb49eb99099a850c2edc76d4039e8bb46aebd14
Guilherme Barboza Mendonça <guilhermebmendonca60@gmail.com>
2023-05-27 23:58:58 -0300

    Correcao no estilo do Header. Header do Dashboard realocado para componente

--- frontend/src/pages/Dashboard.vue
+++ frontend/src/pages/Dashboard.vue
@@ -2,27 +2,20 @@
   <div id="main" class="row">
     <div class="col-12">
 
-      <div id="header" class="row">
-        <div class="col-11">
-          <p class="app-name" style="margin: 0px">
-            Dragon's Den
-          </p>
-        </div>
-        <div class="col-1">
-          <q-btn round color="primary">
-            <q-avatar size="40px" font-size="20px" color="primary" text-color="white">G</q-avatar>
-          </q-btn>
-        </div>
-      </div>
-
-      <hr style="margin: 0px 50px 0px 50px;" />
+      <DashboardHeader />
 
     </div>
   </div>
 </template>
 
 <script>
+
+import DashboardHeader from 'src/components/dashboard/DashboardHeader.vue'
+
 export default {
+  components: {
+    DashboardHeader
+  },
   data () {
     return {
       teste: 'Valor Teste'
@@ -34,16 +27,5 @@ export default {
 <style>
 #main {
   background-color: #E7E7E7;
-}
-
-#header {
-  min-height: 50px;
-  margin: 0px 50px 0px 50px;
-   align-items: center;
-}
-
-.app-name {
-  font-weight: bold;
-  font-size: 30px;
 }
 </style>
