6a498b1d3efe4831d02bff4539f75488cb3a152a
lucas campos <lucascampos.mg@gmail.com>
2023-05-29 19:34:04 -0300

    adiciona api rest que retorna todas as fichas de um usuário de um jogo

--- backend/src/context/game/infra/http/GameRoutes.ts
+++ backend/src/context/game/infra/http/GameRoutes.ts
@@ -1,6 +1,7 @@
 import Server, { HttpMethod } from "../../../../infra/http/Server";
 import HttpRestCreateGame from "./HttpRestCreateGame";
 import HttpRestCreateGameChip from "./HttpRestCreateGameChip";
+import HttpRestGetAllGameChipsByPlayerAndGame from "./HttpRestGetAllGameChipsByPlayerAndGame";
 import HttpRestGetAllPlayersByGame from "./HttpRestGetAllPlayersByGame";
 
 export default class GameRoutes {
@@ -9,5 +10,6 @@ export default class GameRoutes {
         server.on(HttpMethod.POST, "/game", new HttpRestCreateGame());
         server.on(HttpMethod.GET, "/game/:id/players", new HttpRestGetAllPlayersByGame());
         server.on(HttpMethod.POST, "/game/:id/game-chip", new HttpRestCreateGameChip());
+        server.on(HttpMethod.GET, "/game/:id/game-chip", new HttpRestGetAllGameChipsByPlayerAndGame());
     }
 }
