5b49d819fa3db54189ef8371558c099f1ee2be88
Ábner Neves <abnermneves@gmail.com>
2023-05-29 10:25:28 -0300

    password with toggle on sign up

--- frontend/src/pages/Cadastro.vue
+++ frontend/src/pages/Cadastro.vue
@@ -4,8 +4,24 @@
       <p class="dragonsden">Dragon's Den</p>
       <q-form class="q-px-xl column justify-center">
         <q-input dense rounded outlined v-model="username" label="Username" bg-color="grey-4" class="q-mb-lg"/>
-        <q-input dense rounded outlined v-model="password" label="Password" bg-color="grey-4" class="q-mb-lg"/>
-        <q-input dense rounded outlined v-model="confirmationPassword" label="Confirm password" bg-color="grey-4" class="q-mb-lg"/>
+        <q-input dense rounded outlined v-model="password" label="Password" bg-color="grey-4" class="q-mb-lg" :type="isPwd ? 'password' : 'text'">
+          <template v-slot:append>
+            <q-icon
+              :name="isPwd ? 'visibility_off' : 'visibility'"
+              class="cursor-pointer"
+              @click="isPwd = !isPwd"
+            />
+          </template>
+        </q-input>
+        <q-input dense rounded outlined v-model="confirmationPassword" label="Confirm password" bg-color="grey-4" class="q-mb-lg" :type="isCnfPwd ? 'password' : 'text'">
+          <template v-slot:append>
+            <q-icon
+              :name="isCnfPwd ? 'visibility_off' : 'visibility'"
+              class="cursor-pointer"
+              @click="isCnfPwd = !isCnfPwd"
+            />
+          </template>
+        </q-input>
         <q-btn rounded color="cinza" label="Sign up" class="q-mb-lg" @click="cadastrarUsuario()"/>
       </q-form>
       <p class="text-center">Already have an account? <a href="signin" class="link-login text-bold">Log in now</a></p>
@@ -21,7 +37,9 @@ export default {
     return {
       username: '',
       password: '',
-      confirmationPassword: ''
+      confirmationPassword: '',
+      isPwd: true,
+      isCnfPwd: true
     }
   },
   methods: {
