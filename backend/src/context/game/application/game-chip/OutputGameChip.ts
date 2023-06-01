4c3162fc64a3f69324233a8ad3db5ba23e844100
lucas campos <lucascampos.mg@gmail.com>
2023-06-01 09:05:18 -0300

    adiciona dto para output de retorno da ficha de um jogo

---
+++ backend/src/context/game/application/game-chip/OutputGameChip.ts
@@ -0,0 +1,47 @@
+export default class OutputGameChip {
+
+    constructor(
+        readonly id: string,
+        readonly name: string,
+        readonly level: number,
+        readonly clazz: string,
+        readonly stats: GameChipStatsOutput[],
+        readonly inventorys: GameChipInventoryOutput[],
+        readonly skills: GameChipSkillsOutput[],
+        readonly enchantments: GameChipEnchantmentsOutput[], 
+        readonly playersEditPermission: string[]
+    ) {}
+}
+
+export class GameChipStatsOutput {
+    constructor(
+        readonly name: string,
+        readonly value: number
+    ) {}
+};
+
+export class GameChipInventoryOutput {
+    constructor(
+        readonly name: string,
+        readonly quantity: number
+    ) {}
+};
+
+export class GameChipSkillsOutput {
+    constructor(
+        readonly name: string,
+        readonly description: string
+    ) {}
+};
+
+export class GameChipEnchantmentsOutput {
+    constructor(
+        readonly name: string,
+        readonly castingTime: string,
+        readonly range: string,
+        readonly duration: string,
+        readonly concentration: boolean,
+        readonly description: string,
+        readonly level: number
+    ) {}
+};
